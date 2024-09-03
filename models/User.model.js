e00367f1713adea81f1a1ab74e70438f57529ee4
PRINCE ROY <83387075+princeroy8606@users.noreply.github.com>
2024-09-03 11:12:00 +0530

    update -> bcryptjs -> bcrypt

--- models/User.model.js
+++ models/User.model.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 
 
 
 const CoursePurchasedSchema = new mongoose.Schema({
-  
+
   courseDomain: { type: String, required: true },
   courseId: { type: mongoose.Schema.Types.ObjectId, ref: 'Course', required: true },
   packages: [{ type: String, required: true }],
@@ -13,26 +13,26 @@ const CoursePurchasedSchema = new mongoose.Schema({
 });
 
 const userSchema = new mongoose.Schema({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    mobile: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    maritalStatus: { type: String, required: true },
-    dob: { type: Date, required: true },
-    gender: { type: String, required: true },
-    applyingFor: { type: String, required: true },
-    packages: [{ type: String, required: true }],
-    educationalQualification: { type: String, required: true },
-    theologicalQualification: { type: String, required: true },
-    presentAddress: { type: String, required: true },
-    ministryExperience: { type: String, required: true },
-    salvationExperience: { type: String, required: true },
-    signatureFile: { type: String, required: true },  // base64 encoded string
-    photoFile: { type: String, required: true },      // base64 encoded string
-    educationCertificateFile: { type: String, required: true },  // base64 encoded string
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    coursePurchased: [CoursePurchasedSchema],
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  mobile: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  maritalStatus: { type: String, required: true },
+  dob: { type: Date, required: true },
+  gender: { type: String, required: true },
+  applyingFor: { type: String, required: true },
+  packages: [{ type: String, required: true }],
+  educationalQualification: { type: String, required: true },
+  theologicalQualification: { type: String, required: true },
+  presentAddress: { type: String, required: true },
+  ministryExperience: { type: String, required: true },
+  salvationExperience: { type: String, required: true },
+  signatureFile: { type: String, required: true },  // base64 encoded string
+  photoFile: { type: String, required: true },      // base64 encoded string
+  educationCertificateFile: { type: String, required: true },  // base64 encoded string
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  coursePurchased: [CoursePurchasedSchema],
 });
 
 userSchema.pre('save', async function (next) {
